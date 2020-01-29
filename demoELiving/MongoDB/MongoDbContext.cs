faab9abea5621a2e689f9ed5c0fd1acf4162047f
luqman <f168210@nu.edu.pk>
2020-01-29 18:56:32 +0500

    Azure conection string
    Co-Authored-By: luqmanzulfiqarahmed <luqmanzulfiqarahmed@users.noreply.github.com>

--- demoELiving/MongoDB/MongoDbContext.cs
+++ demoELiving/MongoDB/MongoDbContext.cs
@@ -10,23 +10,34 @@ namespace demoELiving.MongoDB
     public class MongoDbContext
     {
 
-        private IMongoDatabase database =null;
-        private static MongoDbContext mongoDb=null;
+        private IMongoDatabase database = null;
+        private static MongoDbContext mongoDb = null;
         private MongoDbContext(IConfiguration config)
         {
+            string connectionString =
+  @"mongodb://luqmanahmed:M8k0cDWJS70KUVUpIaXsY2mLpkSmIaO6EpYhteiUwhIHWxZOboncneoLedxlgTFsG5Joe2lwE8f26x7uvtLQng==@luqmanahmed.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@luqmanahmed@";
+            MongoClientSettings settings = MongoClientSettings.FromUrl(
+              new MongoUrl(connectionString)
+            );
+            settings.SslSettings =
+              new SslSettings() { EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12 };
+            var mongoClient = new MongoClient(settings);
 
-            var client = new MongoClient("mongodb://localhost:27017");
-            database = client.GetDatabase("HousingSocietyAppBuilder");
+
+            database = mongoClient.GetDatabase("HousingSocietyAppBuilder");
 
         }
-        public static MongoDbContext getMongoDbContext(IConfiguration config) {
+        public static MongoDbContext getMongoDbContext(IConfiguration config)
+        {
 
-            if (mongoDb == null) {
+            if (mongoDb == null)
+            {
                 mongoDb = new MongoDbContext(config);
             }
             return mongoDb;
         }
-        public IMongoDatabase getDataBase() {
+        public IMongoDatabase getDataBase()
+        {
             return database;
         }
 
