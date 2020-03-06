270ca3d99027a8a3149d54b06356b079ec4be45b
luqman <f168210@nu.edu.pk>
2020-03-06 18:22:33 +0500

    Update Admin.cs

--- demoELiving/Models/Admin.cs
+++ demoELiving/Models/Admin.cs
@@ -11,19 +11,36 @@ namespace demoELiving.Models
         // [BsonRepresentation(BsonType.ObjectId)]
         // public string Id { get; set; }        //this is internal id of mongodb we have our own 
         // i.e adminId we store unique id by using find before registring any admin 
+
+        
        [BsonElement("adminId")]
         public string adminId { get; set; }
-       [BsonElement("name")]
-        public string name { get; set; }
-      [BsonElement("age")]
-        public string age { get; set; }
-       [BsonElement("gender")]
-        public string gender { get; set; }
+       
+       [BsonElement("adminFirstName")]
+        public string adminFirstName { get; set; }
+      
+      [BsonElement("adminLastName")]
+        public string adminFirstName { get; set; }
+      
+      [BsonElement("adminPassword")]
+        public string adminPassword { get; set; }
+      
+      [BsonElement("adminCnic")]
+        public string adminCnic { get; set; }
+
+      [BsonElement("adminEmail")]
+        public string adminEmail { get; set; }
+      
+      [BsonElement("adminPhoneNo")]
+        public string adminPhoneNo { get; set; }
+      
+      [BsonElement("admindateofBirth")]
+        public string admindateofBirth { get; set; }
+      
       [BsonElement("HousingSocietyID")]
         public string HousingSocietyID { get; set; }
 
-      [BsonElement("password")]
-        public string password { get; set; }
+
         public Admin()
         {
 
