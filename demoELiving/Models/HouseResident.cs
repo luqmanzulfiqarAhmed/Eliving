862e3f871c2cba48f04ec676ff517436c8f2311a
luqman <f168210@nu.edu.pk>
2020-03-08 17:46:45 +0500

    Update HouseResident.cs

--- demoELiving/Models/HouseResident.cs
+++ demoELiving/Models/HouseResident.cs
@@ -10,20 +10,29 @@ namespace demoELiving.Models
        
         [BsonElement("housingSocietyID")]
         public string housingSocietyID { get; set; }
-        [BsonElement("residentName")]
-        public string residentName { get; set; }
-       [BsonElement("residentAge")]
-        public string residentAge { get; set; }
+        [BsonElement("residentFirstName")]
+        public string residentFirstName { get; set; }
+
+        [BsonElement("residentLastName")]
+        public string residentLastName { get; set; }
+        
+        [BsonElement("cnic")]
+        public string cnic { get; set; }
+
+       [BsonElement("residentDateOfBirth")]
+        public string residentDateOfBirth { get; set; }
+        [BsonElement("email")]
+        public string email { get; set; }
+
+        [BsonElement("phoneNo")]
+        public string phoneNo { get; set; }
+        
         [BsonElement("propertyId")]
         public string propertyId { get; set; }
         [BsonElement("address")]
         public string address { get; set; }
         [BsonElement("password")]
         public string password { get; set; }
-        [BsonElement("phone")]
-        public string phone { get; set; }
-        [BsonElement("email")]
-        public string email { get; set; }
         
         public HouseResident(){}
             
